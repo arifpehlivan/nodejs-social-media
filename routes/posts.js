701f05c3c8054d220c1be5da6017e24a4ab91a5f
arifpehlivan <arifpehlivanapap@gmail.com>
2022-03-04 23:41:52 +0300

    add delete a post

--- routes/posts.js
+++ routes/posts.js
@@ -29,6 +29,21 @@ router.put("/:id", async (req,res)=>{
 });
 
 //delete a post
+router.delete("/:id", async (req,res)=>{
+    try {
+        const post= await Post.findById(req.params.id);
+        if (post.userId===req.body.userId) {
+            await post.deleteOne({$set:req.body});
+            res.status(200).json("The post has been deleted");
+        } else {
+            res.status(403).json("You can delete only your post");
+        }
+    } catch (err) {
+        res.status(500).json(err);
+        console.log(err);
+    }
+});
+
 //like a post
 //get a post
 //get timeline posts
