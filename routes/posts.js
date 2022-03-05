28e2e10f33949f988da86f028af8e84c5dceef5a
arifpehlivan <arifpehlivanapap@gmail.com>
2022-03-05 18:54:58 +0300

    get timeline posts

--- routes/posts.js
+++ routes/posts.js
@@ -67,8 +67,23 @@ router.get("/:id", async (req,res)=>{
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
 
 //get timeline posts
+router.get("/timeline/all", async (req,res)=>{
+    try {
+        const currentUser= await User.findById(req.body.userId);
+        const userPosts= await Post.find({userId:currentUser._id});
+        const friendPosts= await Promise.all(
+            currentUser.followings.map(friendId=>{
+               return Post.find({userId:friendId});
+            })
+        );
+        res.json(userPosts.concat(...friendPosts))
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports=router;
